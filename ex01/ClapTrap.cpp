b773e501a1a242bb35d87fdde046757b10efd85e
ismaelucky342 <ismaelhc342@gmail.com>
2025-07-23 21:32:27 +0200

    Commit: 23-07-2025 21:32:27

--- ex01/ClapTrap.cpp
+++ ex01/ClapTrap.cpp
@@ -13,38 +13,49 @@
 #include "ClapTrap.hpp"
 
 ClapTrap::ClapTrap():
-	_name(""), _hitPoints(10), _maxHitPoints(10), _energyPoints(10), _maxEnergyPoints(10), _attackDamage(0)
+	_name(""), _hitPoints(10), _maxHitPoints(10), _energyPoints(10), 
+	_maxEnergyPoints(10), _attackDamage(0)
 {
-	std::cout << GREEN "Claptrap Default constructor called" RESET << std::endl;
+	std::cout << GREEN "Claptrap Default constructor called" RESET 
+	<< std::endl;
 }
 
 
 ClapTrap::ClapTrap(std::string name):
-	_name(name), _hitPoints(10), _maxHitPoints(10), _energyPoints(10), _maxEnergyPoints(10), _attackDamage(0)
+	_name(name), _hitPoints(10), _maxHitPoints(10), _energyPoints(10), 
+	_maxEnergyPoints(10), _attackDamage(0)
 {
-	std::cout << BLUE "ClapTrap first Param constructor called" RESET << std::endl;
+	std::cout << BLUE "ClapTrap first Param constructor called" RESET 
+	<< std::endl;
 }
 
-ClapTrap::ClapTrap(std::string name, unsigned int hitPoints, unsigned int energyPoints, unsigned int attackDamage) :
-	_name(name), _hitPoints(hitPoints), _maxHitPoints(hitPoints), _energyPoints(energyPoints), _maxEnergyPoints(energyPoints), _attackDamage(attackDamage)
+ClapTrap::ClapTrap(std::string name, unsigned int hitPoints, 
+	unsigned int energyPoints, unsigned int attackDamage) :
+	_name(name), _hitPoints(hitPoints), _maxHitPoints(hitPoints), 
+	_energyPoints(energyPoints), _maxEnergyPoints(energyPoints), 
+	_attackDamage(attackDamage)
 {
-	std::cout << CYAN "ClapTrap Second Param constructor called" RESET << std::endl;
+	std::cout << CYAN "ClapTrap Second Param constructor called" RESET 
+	<< std::endl;
 }
 
 ClapTrap::~ClapTrap()
 {
-	std::cout << RED "ClapTrap Destructor called" RESET	 << std::endl;
+	std::cout << RED "ClapTrap Destructor called" RESET	 
+	<< std::endl;
 }
 
 ClapTrap::ClapTrap(ClapTrap const& cl)
 {
-	std::cout << MAGENTA "ClapTrap Copy constructor called" RESET << std::endl;
+	std::cout << MAGENTA "ClapTrap Copy constructor called" RESET 
+	<< std::endl;
 	operator=(cl);
 }
 
 ClapTrap&	ClapTrap::operator=(ClapTrap const& cl)
 {
-	std::cout << YELLOW "ClapTrap Operator = called" RESET << std::endl;
+	std::cout << YELLOW "ClapTrap Operator = called" RESET
+	<< std::endl;
 	_name = cl._name;
 	_hitPoints = cl._hitPoints;
 	_maxHitPoints = cl. _maxHitPoints;
@@ -56,7 +67,9 @@ ClapTrap&	ClapTrap::operator=(ClapTrap const& cl)
 
 void		ClapTrap::display(std::ostream& stream) const
 {
-	stream << BRIGHT_GREEN "ClapTrap " << _name << " has " << _hitPoints << " hit points, " << _energyPoints << " energy points and " << _attackDamage << " attack dammage.";
+	stream << BRIGHT_GREEN "ClapTrap " << _name << " has " << _hitPoints 
+	<< " hit points, " << _energyPoints << " energy points and " 
+	<< _attackDamage << " attack dammage.";
 }
 
 std::ostream&	operator<<(std::ostream& stream, ClapTrap const& cl)
@@ -69,7 +82,9 @@ void 		ClapTrap::attack(std::string const& target)
 {
 	if (_energyPoints >= 5)
 	{
-		std::cout << RED "ClapTrap " << _name << " attacks " << target << ", causing " << _attackDamage << " points of damage! " RESET << std::endl;
+		std::cout << RED "ClapTrap " << _name << " attacks " 
+		<< target << ", causing " << _attackDamage 
+		<< " points of damage! " RESET << std::endl;
 		_energyPoints -= 5;
 	}
 	else
@@ -78,7 +93,8 @@ void 		ClapTrap::attack(std::string const& target)
 
 void 		ClapTrap::takeDamage(unsigned int amount)
 {
-	std::cout << BRIGHT_GREEN "ClapTrap " << _name << " takes " << amount << " damage." RESET << std::endl;
+	std::cout << BRIGHT_GREEN "ClapTrap " << _name 
+	<< " takes " << amount << " damage." RESET << std::endl;
 	if (amount > _hitPoints)
 		_hitPoints = 0;
 	else
@@ -90,7 +106,8 @@ void 		ClapTrap::takeDamage(unsigned int amount)
 
 void		ClapTrap::beRepaired(unsigned int amount)
 {
-	std::cout << BRIGHT_GREEN "ClapTrap " << _name << " is repaired by " << amount << " points." RESET << std::endl;
+	std::cout << BRIGHT_GREEN "ClapTrap " << _name 
+	<< " is repaired by " << amount << " points." RESET << std::endl;
 
 	if (_hitPoints + amount > _maxHitPoints)
 		_hitPoints = _maxHitPoints;
--- ex01/ScavTrap.cpp
+++ ex01/ScavTrap.cpp
@@ -15,35 +15,43 @@
 ScavTrap::ScavTrap() :
 	ClapTrap("", 100, 50, 20)
 {
-	std::cout << GREEN "ScavTrap default constructor called" RESET << std::endl;
+	std::cout << GREEN "ScavTrap default constructor called" RESET 
+	<< std::endl;
 }
 ScavTrap::ScavTrap(std::string name) :
 	ClapTrap(name, 100, 50, 20)
 {
-	std::cout << BLUE "ScavTrap param constructor called" RESET << std::endl;
+	std::cout << BLUE "ScavTrap param constructor called" RESET 
+	<< std::endl;
 }
 
 ScavTrap::~ScavTrap()
 {
-	std::cout << CYAN "ScavTrap destructor called" RESET << std::endl;
+	std::cout << CYAN "ScavTrap destructor called" RESET 
+	<< std::endl;
 }
 
 ScavTrap::ScavTrap(ScavTrap const& cl)
 {
-	std::cout << MAGENTA "ScavTrap operator = called" RESET << std::endl;
+	std::cout << MAGENTA "ScavTrap operator = called" RESET 
+	<< std::endl;
 	operator=(cl);
 }
 
 void		ScavTrap::display(std::ostream& stream) const
 {
-	stream << BRIGHT_GREEN "ScavTrap " << _name << " has " << _hitPoints << " hit points, " << _energyPoints << " energy points and " RESET << _attackDamage << " attack dammage.";
+	stream << BRIGHT_GREEN "ScavTrap " << _name << " has " 
+	<< _hitPoints << " hit points, " << _energyPoints 
+	<< " energy points and " RESET << _attackDamage << " attack dammage.";
 }
 
 void 		ScavTrap::attack(std::string const& target)
 {
 	if (_energyPoints >= 5)
 	{
-		std::cout << BRIGHT_RED "ScavTrap " << _name << " attacks " << target << ", causing " << _attackDamage << " points of damage! " RESET << std::endl;
+		std::cout << BRIGHT_RED "ScavTrap " << _name 
+		<< " attacks " << target << ", causing " << _attackDamage 
+		<< " points of damage! " RESET << std::endl;
 		_energyPoints -= 5;
 	}
 	else
@@ -52,7 +60,8 @@ void 		ScavTrap::attack(std::string const& target)
 
 void		ScavTrap::guardGate()
 {
-	std::cout << YELLOW "ScavTrap have enterred in Gate keeper mode." RESET << std::endl;
+	std::cout << YELLOW "ScavTrap have enterred in Gate keeper mode." RESET 
+	<< std::endl;
 }
 
 std::ostream&	operator<<(std::ostream& stream, ScavTrap const& cl)
